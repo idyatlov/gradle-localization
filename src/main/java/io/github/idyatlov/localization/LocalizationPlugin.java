62a145419505a0d9b0f4dbc7bb27c00dbd0bd613
Dyatlov Ivan <idyatlov@spbtv.com>
2016-08-05 12:43:18 +0300

    initial commit

---
+++ src/main/java/io/github/idyatlov/localization/LocalizationPlugin.java
@@ -0,0 +1,10 @@
+package io.github.idyatlov.localization;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class LocalizationPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+    }
+}
